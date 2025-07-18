b2f3a9832da1526ba689197b8f45bdfe4a08a712
Hulu <hulucat1024@gmail.com>
2025-07-19 05:30:03 +0800

    銝??

--- readv1_8.js
+++ readv1_8.js
@@ -337,7 +337,7 @@ $(".londingshap100").animate({width:'100%'},1000);
 
 setTimeout(() => {
 
-window.location.href = `/magi.html?room=${rooms.roomId}&server=${servertoURL}`
+window.location.href = `${rooms.roomId}`
 
 },1000)
 
